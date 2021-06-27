4306eaa73764f781a5f7d61f623d34a22a44aeca
risuney <honey197692@gmail.com>
2021-06-27 19:03:52 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+<p align="center">
+  <a href="https://risuney.github.io/ramen-timer/">
+    <img src="static/logo.svg" height="200px">
+  </a>
+</p>
+
+# Ramen Timer
+ラーメンたちを美味しく作るためのタイマーです。
+わかりやすいUIで残り時間を視覚的に表すことに成功しました。
+スマートフォンの縦画面表示に対応しています。
+
+# How To Use
+![](https://user-images.githubusercontent.com/77106106/123539936-3417ff00-d777-11eb-94bf-7221277b7862.png)
+ホーム画面です。
+初期状態は一般的な待ち時間`3:00`に設定してあります。
+0以上の整数を入力してください。
+
+`START`をクリックすると
+![](https://user-images.githubusercontent.com/77106106/123539951-44c87500-d777-11eb-8193-dc4f7e583811.png)
+このようにロゴが右回転し、残り時間が表示されます。
+
+タイマーが終了するか、`CAN'T WAIT`をクリックすると
+![](https://user-images.githubusercontent.com/77106106/123539963-5c9ff900-d777-11eb-824f-947417d40b29.png)
+このような画面が表示されます。
+`ONE MORE`をクリックすると最初の画面に戻ります。
---
+++ index.js
@@ -0,0 +1,61 @@
+function clock(n) {
+  var clock;
+  if(n < 10) {
+    clock = '0' + n;
+  } else {
+    clock = n;
+  } return clock;
+}
+
+$('#start').on('click', function(){
+  // inputから取得
+  var min = parseInt($('#set-min').val());
+  var sec = parseInt($('#set-sec').val());
+  var time = min * 60 + sec; // secに変換
+  var mtime = (min * 60 + sec) * 1000; // msecに変換
+  var style = 'r ' + time + 's linear infinite'; // ロゴを回すCSS
+  $('#home').removeClass('active');
+  $('#timer').addClass('active');
+  $('.timer-logo').css('animation', style); // styleを適用
+  // 二桁の形式
+  var time_min = clock($('#set-min').val());
+  var time_sec = clock($('#set-sec').val());
+  $('#r-sec').text(time_sec);
+  $('#r-min').text(time_min);
+  // 終了時
+  setTimeout(function(){
+    $('.timer-logo').css('animation', '');
+    $('#message, #close').addClass('eat');
+    $('#remaining, #wait').addClass('cant-eat');
+  }, time * 1000);
+  // timer
+  const pushtime = Date.now();
+  const endtime = pushtime - mtime; // 終了時の時間
+  const timer = setInterval(function(){
+    time--;
+    var r_min = Math.floor(time / 60); // 分
+    var r_sec = time - r_min * 60; // 秒
+    $('#r-min').text(clock(r_min));
+    $('#r-sec').text(clock(r_sec));
+    mtime = mtime - 1000;
+    var diff = endtime - mtime;
+    // 終了したらタイマー解除
+    if (mtime == 0) {
+      clearInterval(timer)
+    }
+  }, 1000)
+});
+
+$('#wait').on('click', function(){
+  $('.timer-logo').css('animation', '');
+  $('#message, #close').addClass('eat');
+  $('#remaining, #wait').addClass('cant-eat');
+  clearInterval(timer);
+});
+
+$('#close').on('click', function(){
+  $('#message, #close').removeClass('eat');
+  $('#remaining, #wait').removeClass('cant-eat');
+  $('#timer').removeClass('active');
+  $('#home').addClass('active')
+});
