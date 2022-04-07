4e1621ab19131fe2df8e7192687f15e850f8e64c
risuney <honey197692@gmail.com>
2022-04-07 14:02:00 +0900

    fix: Fix bugs

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ function clock(n) {
   } return clock;
 }
 
+//スタートをクリック
 $('#start').on('click', function(){
   // inputから取得
   var min = parseInt($('#set-min').val());
   var sec = parseInt($('#set-sec').val());
   var time = min * 60 + sec; // secに変換
-  var mtime = (min * 60 + sec) * 1000; // msecに変換
-  var style = 'r ' + time + 's linear infinite'; // ロゴを回すCSS
+  var mtime = (min * 60 + sec) * 1000; // millisecに変換
+  var style = 'r ' + time + 's linear infinite'; // ラーメンを回すスタイル
   $('#home').removeClass('active');
   $('#timer').addClass('active');
-  $('.timer-logo').css('animation', style); // styleを適用
+  $('.timer-logo').css('animation', style); // ラーメンを回転し始める
   // 二桁の形式
   var time_sec = clock($('#set-sec').val());
   $('#r-min').text(min);
@@ -42,13 +43,15 @@ $('#start').on('click', function(){
     if (mtime == 0) {
       clearInterval(timer)
     }
-  }, 1000)
-});
+  }, 1000);
 
-$('#wait').on('click', function(){
-  $('.timer-logo').css('animation', '');
-  $('#message, #close').addClass('eat');
-  $('#remaining, #wait').addClass('cant-eat');
+  //待ちきれない場合
+  $('#wait').on('click', function() {
+    $('.timer-logo').css('animation', ''); //ラーメンの回転を停止
+    $('#message, #close').addClass('eat'); //YOU CAN EAT! を表示
+    $('#remaining, #wait').addClass('cant-eat');
+    clearInterval(timer); //タイマーを解除
+  });
 });
 
 $('#close').on('click', function(){
